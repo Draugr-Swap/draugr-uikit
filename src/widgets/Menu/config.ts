7d4530eb879207473a52aa07bed1a897d9725ce0
Naoki Murano <naokimurano1996@gmail.com>
2022-01-20 22:55:05 +0900

    init commit

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,93 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://exchange.draugrswap.finance",
+      },
+      {
+        label: "Liquidity",
+        href: "https://exchange.draugrswap.finance/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://pancakeswap.info",
+      },
+      {
+        label: "Tokens",
+        href: "https://pancakeswap.info/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://pancakeswap.info/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://pancakeswap.info/accounts",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/DraugrDefi/",
+      },
+      {
+        label: "Docs",
+        href: "https://draugrswap.gitbook.io/draugr-swap/",
+      },
+      {
+        label: "Blog",
+        href: "https://draugr-swap.medium.com/",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/DraugrSwapOfficial",
+      },
+      {
+        label: "Announcements",
+        href: "https://t.me/draugrswapannouncements",
+      },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/draugrswap",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
