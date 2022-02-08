5fb62f22ca62851a4dc132b42a3bddfaf7830f12
Naoki Murano <naokimurano@outlook.com>
2022-02-09 00:26:25 +0900

    update button color

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2635,7 +2635,7 @@ var baseColors = {
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#4D4F96", backgroundDisabled: "#5555A3", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#4D4F96", tertiary: "#4D4F96", text: '#F25A29', textDisabled: '#998c83', textSubtle: '#F25A29', borderColor: "#E9EAEB", card: "#4D4F96", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#4D4F96", backgroundDisabled: "#F25A29", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#4D4F96", tertiary: "#F25A29", text: '#F25A29', textDisabled: '#998c83', textSubtle: '#F25A29', borderColor: "#E9EAEB", card: "#4D4F96", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
 var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#be7434", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", 
